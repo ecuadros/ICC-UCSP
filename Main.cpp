ec30c1af89609702198808a40992622b799c7ae4
Ernesto Cuadros-Vargas <ecuadros@gmail.com>
2025-11-08 13:45:00 -0500

    Update TODO comments for clarity

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+using namespace std;
 
 int main(){
-    //TODO: Rafael agregar aqui
+    //TODO: Rafael agregar aqui (suma)
 
-    // TODO Rodrigo Molina P Agregar aqui
+    // TODO Rodrigo Molina P Agregar aqui (multiplicar)
 
 
     return 0;
