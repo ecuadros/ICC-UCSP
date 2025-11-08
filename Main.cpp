c05e345356f2cda755a87bfe84ee4d6040b6604f
RodrigoMolinaP <rodrigomolina9797@gmail.com>
2025-11-08 14:38:39 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int multiplicar(int a, int b){
+    return a* b;
+}
 int main(){
     //TODO: Rafael agregar aqui (suma)
 
     // TODO Rodrigo Molina P Agregar aqui (multiplicar)
-
+    int resultado = multiplicar(3, 4);
+    cout << "El resultado es: " <<
+resultado << endl;
 
     return 0;
 }
